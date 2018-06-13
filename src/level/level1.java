c7609dda46caafd7e82a3004e102194bdae6e8ec
refael <rapooil120@gmail.com>
2018-06-13 17:09:45 +0300

    background

--- src/level/level1.java
+++ src/level/level1.java
@@ -5,14 +5,16 @@ import java.awt.*;
 
 public class level1 extends JPanel{
     private JFrame window =new JFrame();
-
+    private ImageIcon background = new ImageIcon("imag//Wiki-background.jpg");
     public level1(){
         window.setBounds(200, 100, 500, 350);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
-
+        window.add(this);
     }
     public void paint(Graphics g){
-
+        super.paint(g);
+        g.drawImage(background.getImage(),0,0,getWidth(),getHeight(),null);
+        System.out.println("paint level 1 is called");
     }
 }
--- src/menus/SecondMenu.java
+++ src/menus/SecondMenu.java
@@ -3,6 +3,7 @@ package menus;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import level.level1;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,16 +12,23 @@ import javax.swing.JLabel;
 public class SecondMenu extends JFrame implements ActionListener{
 
 	JButton back;
-	
+	JButton level1;
+
+
 	public SecondMenu (){
 		setBounds(200, 100, 500, 350);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		level1 = new JButton("level 1");
 		back=new JButton("Main Menu");
+		level1.setBounds(130, 270, 100, 20);
 		back.setBounds(20, 270, 100, 20);
 		getContentPane().add(back);
+		getContentPane().add(level1);
+		level1.addActionListener(this);
 		back.addActionListener(this);
 		back.setFocusable(false);
+		level1.setFocusable(false);
 		JLabel label = new JLabel("PACMAN");
 		label.setFont(new Font("Chiller", Font.BOLD ,44));
 		label.setBounds(170, 10, 200, 80);
@@ -32,7 +40,10 @@ public class SecondMenu extends JFrame implements ActionListener{
 			new MainWindow();
 			dispose();
 		}
-		
+		if (e.getSource().equals(level1)){//return to the main menu when button back is pressed.
+			new level1();
+
+		}
 	}
 
 }
